13082bd9303aacc58661480c31565e0ac5841343
Haruto-x-Okura <harutoxokura15@gmail.com>
2026-06-13 13:38:00 +0530

    program to compute compound interest

---
+++ src/compInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class  compInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the principal amount");
+        double p = sc.nextDouble();
+        System.out.println("Enter the interest rate");
+        double r = sc.nextDouble();
+        System.out.println("Enter the number of compounded per year");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of years");
+        int t = sc.nextInt();
+        double amt = p* Math.pow(1+ r/n, n*t);
+        System.out.println("The amount is "+amt);
+    }
+}
